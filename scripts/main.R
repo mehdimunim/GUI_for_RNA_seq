7b0619a032e8c5acb0335026cca793932ff61bcf
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2022-01-24 10:39:19 +0100

    Update RPKM

--- scripts/main.R
+++ scripts/main.R
@@ -1,4 +1,4 @@
-# Main Script 
+# Main Script
 rm(list = ls())
 source("read_data.R")
 source("rpkm.R")
@@ -18,20 +18,27 @@ length_table <- read_length_table(annot_path)
 mapping_table <- read_mapping_table(mapping_table_path)
 
 ### RNA-seq reads
-path1 <- paste(directory,"expression/glucose/24/GSM2188043_expression_1.tsv",sep ="/")
-path2 <- paste(directory,"expression/glucose/48/GSM2188049_expression_19.tsv",sep ="/")
+path1 <-
+  paste(directory,
+        "expression/glucose/24/GSM2188043_expression_1.tsv",
+        sep = "/")
+path2 <-
+  paste(directory,
+        "expression/glucose/48/GSM2188049_expression_19.tsv",
+        sep = "/")
 
 ### read counts and group in two tables
 count_table1 <- read_count_table(path1)
 count_table2 <- read_count_table(path2)
-table_24 <- get_condition_table(count_table1, length_table, mapping_table)
-table_48 <- get_condition_table(count_table2, length_table, mapping_table)
+table_24 <-
+  get_condition_table(count_table1, length_table, mapping_table)
+table_48 <-
+  get_condition_table(count_table2, length_table, mapping_table)
 
 ### RPKM
 rpkm_24 <- rpkm(table_24)
 rpkm_48 <- rpkm(table_48)
 
-#plot_rpkm(rpkm_24, "RPKM test glucose 24 48 rep1", c = "grey")
-plot_rpkm_comparison(rpkm_24, rpkm_48, "RPKM test glucose 24 48 rep1")
-
-
+#plot_rpkm(rpkm_24, "RPKM test glucose 24 rep1", c = "grey")
+#plot_rpkm(rpkm_48, "RPKM test glucose 48 rep1", c = "blue")
+plot_lrpkm_comparison(rpkm_24, rpkm_48, "RPKM test glucose 24 48 rep1")
--- scripts/rpkm.R
+++ scripts/rpkm.R
@@ -1,47 +1,61 @@
-if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(tidyverse))
+  install.packages("tidyverse")
 library(tidyverse)
 
 rpkm <- function(condition_table) {
-  # Get the average RPKM distributions 
+  # Get the average RPKM distributions
   # counts: number of reads per gene
   # lengths: lengths of the genes in counts
   # RPKM = numberOfReads / ( geneLength/1000 * totalNumReads/1,000,000 )
   counts <- as.numeric(condition_table$count)
   lengths <- as.numeric(condition_table$gene_length)
-  rpk <- counts/(lengths/1000)
+  rpk <- counts / (lengths / 1000)
   coef <- sum(rpk) / 1e6
-  return(rpk/coef)
-}  
-
-plot_rpkm <- function(rpkm, title, save = FALSE, output_folder ="", c = "grey") {
-  # Plot an RPKM distribution
-  # if save is true, save graph in the output_folder
-  hist(log(rpkm), 
-       main = title,
-       col = c,
-       xlab = "Log(RPKM)",
-       ylab = "Density of log(RPKM)",
-       breaks = length(rpkm)/100,
-       freq = FALSE
-       )
-  lines(density(log(rpkm)), col = "red", lwd = 3)
+  return(rpk / coef)
 }
 
-plot_rpkm_comparison <- function(rpkm1, rpkm2, title, save = FALSE, output_folder ="") {
+plot_lrpkm <- function(rpkm,
+                       title,
+                       c1 = "lightgrey",
+                       c2 = "red",
+                       add = FALSE) {
+  # Plot a Log RPKM distribution
+  
+  # Create histogram with log(rpkm)
+  lrpkm <- log(rpkm)
+  h <- hist(lrpkm,
+            breaks = length(rpkm) / 100,
+            plot = FALSE)
+  
+  # Create curve from the counts
+  multiplier <- h$counts / h$density
+  d <- density(lrpkm)
+  d$y <- d$y * multiplier[1]
+  
+  # plot with graphical options
+  plot(
+    h,
+    main = title,
+    density = 10,
+    col = c1,
+    xlab = "Log(RPKM)",
+    ylab = "Density of log(RPKM)",
+    add = add
+  )
+  lines(d, col = c2, lwd = 2)
+}
+
+plot_lrpkm_comparison <- function(rpkm1,
+                                 rpkm2,
+                                 title,
+                                 save = FALSE,
+                                 output_folder = "") {
   # Plot and compare RPKM distributions
   # if save is true, save graph in the output_folder
-  df1 <- data.frame(rpkm1)
-  df2 <- data.frame(rpkm2)
-  df1$id <- 1
-  df2$id <- 2
-  data <- rbind(df1, df2)
-  ggplot(data) +
-    aes(1)
-    geom_histogram(bins = 100, col ="blue") +
-    geom_histogram(data = df2, bins = 100, col ="red") +
-    labs(title = title)
-  if (save) { 
-    filename <- "rpkm_output_" + title + ".pdf"
-    ggsave(filename, path = output_folder, dpi=500)
-  }
+  plot_lrpkm(rpkm1, title, c1 = "grey", c2 = "blue")
+  plot_lrpkm(rpkm2,
+            "",
+            c1 = "grey",
+            c2 = "red",
+            add = TRUE)
 }
